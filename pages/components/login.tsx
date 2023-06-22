e9f0534f16e30319baa97ecd49504553f51c2a34
Brishna Bakshev <b.bakshev@gmail.com>
2023-06-22 14:49:40 -0700

    updates

---
+++ pages/components/login.tsx
@@ -0,0 +1,20 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+import { Session } from "next-auth"; // Import Session type from 'next-auth'
+
+export default function Login() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user?.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  )
+}
